b1736b85f2f86a128b2fb2ed03f3ba7827bc0cac
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2019-10-02 09:01:59 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+counter = 0
+flowers = ["rose", "lotus", "daisy", "lily", "peonie"]
+ 
+while flowers[counter] do
+  puts flowers[counter]
+  counter += 1
 end
